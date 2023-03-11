91d6b86931fd2022eda24837513c4b4471113340
pabramber01 <pramosb01@gmail.com>
2023-03-11 14:14:34 +0100

    (fix) Logout reset state
    Now when performing a logout action, global state is reseted.

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { authFormSlice } from './auth';
 import { userShowSlice } from './user';
 import {
@@ -7,14 +7,29 @@ import {
   publicationFormSlice,
 } from './publication';
 
+const combinedReducer = combineReducers({
+  authForm: authFormSlice,
+  userShow: userShowSlice,
+  publicationList: publicationListSlice,
+  publicationShow: publicationShowSlice,
+  publicationForm: publicationFormSlice,
+});
+
+const rootReducer = (state, action) => {
+  const resetActions = [
+    'authForm/logoutUser/fulfilled',
+    'authForm/logoutUserLocal',
+  ];
+
+  if (resetActions.includes(action.type)) {
+    state = undefined;
+  }
+
+  return combinedReducer(state, action);
+};
+
 const store = configureStore({
-  reducer: {
-    authForm: authFormSlice,
-    userShow: userShowSlice,
-    publicationList: publicationListSlice,
-    publicationShow: publicationShowSlice,
-    publicationForm: publicationFormSlice,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
