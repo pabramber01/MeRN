4f22e0572ec1be57282526a024bd98dfd0cc9632
pabramber01 <pramosb01@gmail.com>
2023-04-15 21:43:36 +0200

    (fix) Deploy
    Fixing deploy

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -19,29 +19,23 @@ const attachLoginCookie = ({ res, userToken }) => {
   });
 
   const oneDay = 1000 * 60 * 60 * 24;
-  console.log(process.env.FT_BASE_URL);
-  const domain = process.env.FT_BASE_URL.split(/\.(.*)/s)[1];
 
   res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'Lax',
-    domain: process.env.NODE_ENV === 'production' ? domain : 'localhost',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'Lax',
   });
 };
 
 const attachLogoutCookie = ({ res }) => {
   const oneSec = 1000;
 
-  const domain = process.env.FT_BASE_URL.split(/\.(.*)/s)[1];
-
   res.cookie('token', 'logout', {
     httpOnly: true,
     expires: new Date(Date.now() + oneSec),
-    sameSite: 'Lax',
-    domain: process.env.NODE_ENV === 'production' ? domain : 'localhost',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'Lax',
   });
 };
 
