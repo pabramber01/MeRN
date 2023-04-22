5e7dd4cf412e9bfc7bb2707fe09c4b5b7142238d
pabramber01 <pramosb01@gmail.com>
2023-04-22 12:29:11 +0200

    (fix) Deploy
    Fixing deploy

--- server/src/utils/populate.js
+++ server/src/utils/populate.js
@@ -124,7 +124,7 @@ const copyDirsCloud = async (params) => {
     });
 
     for (const file of files) {
-      console.log(file);
+      console.log(file.split(p.basename(path)));
       const extraPath = file.split(p.basename(path))[1].replaceAll('\\', '/');
       const i = extraPath.lastIndexOf('/');
 
